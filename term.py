3aae20ac639a8c489ede5eeb620c6f4b320f27d4
sharkzy404 <94655227+sharkzy404@users.noreply.github.com>
2023-12-01 19:52:49 +0100

    Update term.py

--- term.py
+++ term.py
@@ -24,36 +24,32 @@ class shark:
 
     def main(self):
         data = """
-                   "Welcome To Mr.Shark Terminal"
+                   Welcome To Mr.Shark Terminal"
                    For help and functions: @help """
-        print (F.GREEN+data)
+        print (F.YELLOW+data)
 
     def help(self):
         tools = """
-[1]. Getting Ip address: @get -ip [target]
-     Example: @get -ip google.com
-[2]. Port scanning: @port -scan [target]
-     Example: @port -scan 127.0.0.1
-[3]. Convert Number to Binary: @num -b [number] [base]
-     Example: @num -b 2000 2
-[4]. Convert Binary to Number: @bina -n [binary] [base]
-     Example: @bina -n 1011101010 2
-[5]. Convert Alphabet to Binary: @alpha -b
-     Example: @alpha -b
-[6]. Convert Binary to Alphabet: @bina -a
-     Example: @bina -a
+[1]. Getting Ip address: get -ip [target]
+     Example: get -ip google.com
+[2]. Port scanning: port -scan [target]
+     Example: port -scan 127.0.0.1
+[3]. Convert Number to Binary: num -b [number] [base]
+     Example: num -b 2000 2
+[4]. Convert Binary to Number: bina -n [binary] [base]
+     Example: bina -n 1011101010 2
+[5]. Convert Alphabet to Binary: alpha -b [alphabet]
+     Example: alpha -b hello|dear|are|you||ok
+     Note: use <|> as spacing..!!
+[6]. Convert Binary to Alphabet: bina -a [binary]
+     Example: bina -a 10101011|10101110|1010101|10101010
+     Note: user <|> as spacing..!!
 [7]. To get device IP address: @ip -details
      Example: @ip -details
 [8]. To get cpu info: @cpu
      Example: @cpu
 [9]. To open server: @open -server
      Example: @open -server
-     Note: multiple servers cant be opened at once,
-           The program is going to close and restart,
-           If trying to open another server after force closing,
-           A recent connection.
-     Note: The program should be named term.py: because thats the file,
-           Its going to re-open, you can modify it....
 [10].To create file: @file <option> <file_name>
      Options: -C create file
               -A append data to existsing file
@@ -65,7 +61,7 @@ class shark:
 [11].To send message to a whatsapp contact: @send -w <number>
     Example: @send -w +1234567890
 
-[12]. To exit the program: @exit
+[12]. To exit program: @exit
 
 MORE Functions COMING...
 
@@ -77,32 +73,21 @@ MORE Functions COMING...
             data = self.soc.gethostbyname(host)
             print (F.BLUE+f"{host}: {data}")
         except:
-            print (F.RED+"[*] Error, maybe invalid host or no network connection [*]")
+            print (F.RED+"[*] Error, maybe invalid host or no network conne
+ction [*]")
 
     def port_scan(self, ip): #2
-        try:
-            total_port = 0
-            port = 0
-            self.socket.settimeout(0.05)
-            for i in range(65536):
+        total_port = 0
+        port = 1
+        for i in range(9999):
+            try:
                 port += 1
-                try:
-                    self.socket.connect((ip, port))
-                    total_port += 1
-                    print (F.BLUE+f"Port {port} opened for {ip}")
-                    self.socket.close()
-                    self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-                    self.socket.settimeout(0.05)
-                except KeyboardInterrupt:
-                    break
-                except:
-                    self.socket.close()
-                    self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREM)
-                    self.socket.settimeout(0.05)
-                    pass
-            print (F.YELLOW+f" [*]Total port opened for {ip} is : {total_port} ")
-        except KeyboardInterrupt:
-            pass
+                self.socket.connect((ip, port))
+                total_port += 1
+                print (F.BLUE+f"port {port} opened for {ip}")
+            except:
+                pass
+        print (F.GREEN+f"total port opened for {ip} is : {total_port}")
 
     def Bina_Num(self, binary, base): #3
         try:
@@ -118,23 +103,24 @@ MORE Functions COMING...
         except:
             print (F.RED+"[*] an error occured [*]")
 
-    def Alpha_Bina(self): #5
-        alph = input(F.BLUE+"[*] data: ").split(" ")
+    def Alpha_Bina(self, alph): #5
+        alph = alph.split("|")
         num = -1
         try:
             while True:
                 num += 1
                 try:
                     for data in bytearray(alph[num], "utf-8"):
-                        print (format(data, "b"))
+                        print (format(data, "b"), end = " ")
+                    print ("\n")
                 except:
                     break
         except:
             print (F.RED+"[*] an error occured [*]")
 
-    def Bina_Alpha(self): #6
+    def Bina_Alpha(self, bina): #6
         try:
-            splita = input(F.BLUE+"[*] data: ").split(" ")
+            splita = bina.split("|")
             for data in splita:
                 data1 = int(data, 2)
                 charac = chr(data1)
@@ -149,7 +135,7 @@ MORE Functions COMING...
             sock.connect(('192.255.255.255',1))
             print (F.BLUE+sock.getsockname()[0])
         except:
-            print(F.BLUE+sub.getoutput('ifconfig').split(" ")[13])
+            print(F.BLUE+sub.getoutput('ifconfig').split(" ")[22])
 
     def cpu_info (self): #8
         try:
@@ -158,45 +144,36 @@ MORE Functions COMING...
             print ("[*] an error occured [*]")
 
     def open_server(self): #9
-        tm.sleep(2)
         print (F.YELLOW+"[*] Starting Server")
-        print (F.GREEN+"[*] To close connection Ctrl -C")
 
         tm.sleep(2)
-        a1, a2, a3 = str(rd.randint(1,6)), str(rd.randint(1,6)), str(rd.randint(1,6))
+        a1, a2, a3 = str(rd.randint(1,6)), str(rd.randint(1,6)), str(rd.ran
+dint(1,6))
         ip = sub.getoutput('ifconfig').split(" ")[13]
         port = a3+a2+a1+a2+a3
-        try:
-            self.sock.bind(("0.0.0.0", int(port)))
-            print (F.BLUE+"Server Started")
-            tm.sleep(1)
-            print (F.GREEN+f"IP: {ip}: PORT: {port}")
+        print (F.BLUE+"Server Started")
 
-            self.sock.listen(5)
+        tm.sleep(1)
+        print (F.GREEN+f"IP: {ip}: PORT: {port}")
+        self.sock.bind(("0.0.0.0", int(port)))
+        self.sock.listen(5)
+        c, addr = self.sock.accept()
 
-            c, addr = self.sock.accept()
+        while True:
+            c.send(F.GREEN+"Waiting for incoming Message::\n".encode())
+            out_mes = input("[*]Send-Message: ")
+            if out_mes == "bye":
+                c.close()
+                quit(0)
+            c.send(f"Receiced message: {out_mes}\n[*]Send-Message: ".encode
+())
+            print ("Message Sent\nWaiting for incoming Message::")
 
-            while True:
-                c.send("Waiting for incoming Message::\n".encode())
-                out_mes = input("[*]Send-Message: ")
-                if out_mes == "bye":
-                    c.close()
-                    break
-                c.send(f"Receiced message: {out_mes}\n[*]Send-Message: ".encode())
-                print ("Message Sent\nWaiting for incoming Message::")
-
-                inp_mes = c.recv(1024).decode()
-                print (f"[*]Received-Message: {inp_mes}")
-                if inp_mes == "bye":
-                    c.close()
-                    break
-        except KeyboardInterrupt:
-            print (F.RED+" [*] Forced Closed [*]")
-        except OSError:
-            print (F.RED+"[*]Socket already opened: Closing and RestartingProgram in 2secs [*]")
-            tm.sleep(2)
-            sys("python term.py")
-
+            inp_mes = c.recv(1024).decode()
+            print (f"[*]Received-Message: {inp_mes}")
+            if inp_mes == "bye":
+                c.close()
+                quit(0)
 
     def file_sys(self, option, file): #10
         if option == "-C":
@@ -235,9 +212,9 @@ MORE Functions COMING...
         elif option == "-D":
             if exists(file):
                 os.remove(file)
-                print ("File deletes successfully")
+                print ("[*] File deleted ")
             else:
-                print ("File doesnt exists")
+                print ("File doesnt existss")
         elif option == "-V":
             if exists(file):
                 print ("File exists")
@@ -254,16 +231,17 @@ MORE Functions COMING...
                 open_file = open(file, "r")
                 data = open_file.read()
                 open_file.close()
-                reg = re. compile( '[@_!#$%^&*()<>?/\|}{~:]' )
-                if reg.search(data) != None:
+                reg = re.search(r"enc=", data)
+                if reg != None:
                     print ("File is in encrypted format\nWish to decrypt")
                     opt = input("Y/N: ").upper()
                     if opt == "Y":
                         decryp_hash = str(0000)
-                        data1 = data.encode()
-                        rep = len(data)-1//len(decrp_hash)+1
+                        data1 = data.replace("enc=", "").encode()
+                        rep = len(data)-1//len(decryp_hash)+1
                         a4 = (decryp_hash*rep)[:len(data)].encode()
-                        new_data = bytes([i1^i2 for (i1,i2) in zip(data1, a4)])
+                        new_data = bytes([i1^i2 for (i1,i2) in zip(data1, a
+4)])
                         dec_data = new_data.decode()
                         rep_data = dec_data.replace("~", " ")
                         new_file = open(file, "w")
@@ -277,7 +255,7 @@ MORE Functions COMING...
                     else:
                         print ("Error, invalid input")
 
-                elif reg.search(data) == None:
+                elif reg == None:
                     print ("File is in decrypted Format\nWish to encrypt")
                     opt = input("Y/N: ").upper()
                     if opt == "Y":
@@ -285,7 +263,8 @@ MORE Functions COMING...
                         data1 = data.replace(" ", "~").encode()
                         rep = len(data)-1//len(decryp_hash)+1
                         a4 = (decryp_hash*rep)[:len(data)].encode()
-                        new_data = bytes([i1^i2 for (i1,i2) in zip(data1, a4)])
+                        new_data =b"enc=" + bytes([i1^i2 for (i1,i2) in zip
+(data1, a4)])
                         new_file = open(file, "w")
                         new_file.write(new_data.decode())
                         new_file.close()
@@ -308,28 +287,22 @@ MORE Functions COMING...
         except:
             print ("Error occured")
 
-    def exit(self): #12
-        print (F.BLUE+"[*] QUITING PROGRAM [*]" )
-        tm.sleep(1.5)
-        quit(0)
-
-
 shark = shark()
 if __name__ == '__main__':
     shark.main()
     while True:
         data = inpu()
-        if "@get -ip" in data: #1
+        if "get -ip" in data: #1
             shark.get_ip(data.split()[2])
-        elif "@port -scan" in data: #2
+        elif "port -scan" in data: #2
             shark.port_scan(data.split()[2])
-        elif "@bina -a" in data: #3
-            shark.Bina_Alpha()
-        elif "@alpha -b" in data: #4
-            shark.Alpha_Bina()
-        elif "@num -b" in data: #5
+        elif "bina -a" in data: #3
+            shark.Bina_Alpha(data.split()[2])
+        elif "alpha -b" in data: #4
+            shark.Alpha_Bina(data.split()[2])
+        elif "num -b" in data: #5
             shark.Num_Bina(data.split()[2], data.split()[3])
-        elif "@bina -n" in data: #6
+        elif "bina -n" in data: #6
             shark.Bina_Num(data.split()[2], data.split()[3])
         elif data == "@ip -details": #7
             shark.get_device_ip()
@@ -343,7 +316,7 @@ if __name__ == '__main__':
             shark.send_mess(data.split()[2])
         elif "@help" in data:
             shark.help()
-        elif data == "@exit": #12
-            shark.exit()
+        elif "@exit" in data:
+            quit(0)
         else:
             sys(data)
