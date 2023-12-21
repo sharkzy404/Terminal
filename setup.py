7cc2f0eacdf0b9701e13c68cfbf01ad9665fdcda
sharkzy404 <ibrababdulshakur2003@gmail.com>
2023-12-21 23:41:09 +0100

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+from os import system as sys
+
+try:
+    sys("pip install colorama")
+except:
+    print ("[*] An error occured")
+
+path_to_pro = input("[*]::'/path/to/term.py': ")
+path_to_bin = input("[*]::'/path/to/bin': ")
+
+try:
+    sys(f"chmod +x {path_to_pro}")
+    sys(f"ln -s {path_to_pro} {path_to_bin}/term")
+    print ("[*] Done: Start Terminal By Inputting : 'term'")
+except:
+    print ("[*] An Error Occured")
+
+
+
+
